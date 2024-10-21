cec7768fd44e50e6ac0692bd022c5946fbaf7929
Alejandro Moreo <alejandro.moreo@isti.cnr.it>
2024-10-21 13:41:14 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,162 @@
+import numpy as np
+import pandas as pd
+import quapy as qp
+import quapy.functional as F
+from quapy.protocol import UPP, NPP
+from quapy.method.aggregative import PACC, EMQ, KDEyML
+from model import WithCIAgg
+import pickle
+import os
+from time import time
+from collections import defaultdict
+from sklearn.linear_model import LogisticRegression
+from quapy.model_selection import GridSearchQ
+from pathlib import Path
+
+SEED = 1
+
+
+def newLR():
+    return LogisticRegression(max_iter=3000)
+
+
+# typical hyperparameters explored for Logistic Regression
+# logreg_grid = {
+#     'C': np.logspace(-4,4,9),
+#     'class_weight': [None, 'balanced']
+# }
+
+
+# def wrap_hyper(classifier_hyper_grid: dict):
+#     return {'classifier__' + k: v for k, v in classifier_hyper_grid.items()}
+
+
+METHODS = [
+    # ('PACC-99', WithCIAgg(PACC(newLR()), confidence_level=0.99), {}), # wrap_hyper(logreg_grid)),
+    ('PACC-95-1', WithCIAgg(PACC(newLR()), confidence_level=0.95, sample_size=1.), {}), # wrap_hyper(logreg_grid)),
+    # ('PACC-90', WithCIAgg(PACC(newLR()), confidence_level=0.90), {}), # wrap_hyper(logreg_grid)),
+    # ('SLD-99', WithCIAgg(EMQ(newLR()), confidence_level=0.99), {}), # wrap_hyper(logreg_grid)),
+    ('SLD-95-1', WithCIAgg(EMQ(newLR()), confidence_level=0.95, sample_size=1.), {}), # wrap_hyper(logreg_grid)),
+    # ('SLD-90', WithCIAgg(EMQ(newLR()), confidence_level=0.90), {}), # wrap_hyper(logreg_grid)),
+    # ('KDEy',  KDEyML(newLR()), {**wrap_hyper(logreg_grid), **{'bandwidth': np.logspace(-4, np.log10(0.2), 20)}}),
+]
+
+
+
+def show_results(result_path):
+    import pandas as pd
+    df = pd.read_csv(result_path + '.csv', sep='\t')
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.width', 1000)  # Ajustar el ancho máximo
+    pv = df.pivot_table(index='Dataset', columns="Method", values=["MAE"], margins=True)
+    print(pv)
+    pv = df.pivot_table(index='Dataset', columns="Method", values=["MRAE"], margins=True)
+    print(pv)
+    pv = df.pivot_table(index='Dataset', columns="Method", values=["SUCCESS"], margins=True)
+    print(pv)
+    pv = df.pivot_table(index='Dataset', columns="Method", values=["TR-TIME"], margins=True)
+    print(pv)
+    pv = df.pivot_table(index='Dataset', columns="Method", values=["TE-TIME"], margins=True)
+    print(pv)
+
+
+def run_experiment(method_name, quantifier, param_grid):
+
+    print('Init method', method_name)
+
+    with open(global_result_path + '.csv', 'at') as csv:
+        for dataset in qp.datasets.UCI_MULTICLASS_DATASETS:
+            print('init', dataset)
+
+            local_result_path = os.path.join(Path(global_result_path).parent, method_name + '_' + dataset + '.dataframe')
+            if os.path.exists(local_result_path):
+                print(f'result file {local_result_path} already exist; skipping')
+                report = qp.util.load_report(local_result_path)
+
+            else:
+                with qp.util.temp_seed(SEED):
+
+                    data = qp.datasets.fetch_UCIMulticlassDataset(dataset, verbose=True)
+                    train, test = data.train_test
+
+                    if len(param_grid) == 0:
+                        t_init = time()
+                        quantifier.fit(train)
+                        train_time = time() - t_init
+                    else:
+                        # model selection (train)
+                        train, val = train.split_stratified(random_state=SEED)
+                        protocol = UPP(val, repeats=n_bags_val)
+                        modsel = GridSearchQ(
+                            quantifier, param_grid, protocol, refit=True, n_jobs=-1, verbose=1, error='mae'
+                        )
+                        t_init = time()
+                        try:
+                            modsel.fit(train)
+                            print(f'best params {modsel.best_params_}')
+                            print(f'best score {modsel.best_score_}')
+                            quantifier = modsel.best_model()
+                        except:
+                            print('something went wrong... trying to fit the default model')
+                            quantifier.fit(train)
+                        train_time = time() - t_init
+
+                    # test
+                    t_init = time()
+
+                    row_entries = []
+                    protocol = UPP(test, repeats=n_bags_test)
+                    errs, success, = [], []
+                    for i, (sample, true_prev) in enumerate(protocol()):
+                        confidence_region = quantifier.quantify_ci(sample)
+                        err_mae = qp.error.ae(true_prev, confidence_region.mean)
+                        err_mrae = qp.error.rae(true_prev, confidence_region.mean)
+                        is_within = confidence_region.within(true_prev)
+
+                        series = {
+                            'true-prev': true_prev,
+                            'estim-prev': confidence_region.mean,
+                            'mae': err_mae,
+                            'mrae': err_mrae,
+                            'within': is_within,
+                        }
+                        row_entries.append(series)
+
+                        errs.append(err_mae)
+                        success.append(is_within*1)
+
+                        print(f'[{(i+1)/n_bags_test}] MAE={np.mean(errs):.4f} Success={np.mean(success):.2f}%')
+
+                    report = pd.DataFrame.from_records(row_entries)
+
+                    test_time = time() - t_init
+                    report['tr_time'] = train_time
+                    report['te_time'] = test_time
+                    report.to_csv(local_result_path)
+
+            means = report.mean(numeric_only=True)
+            csv.write(f'{method_name}\t{dataset}\t{means["mae"]:.5f}\t{means["mrae"]:.5f}\t{means["within"]:.2f}\t{means["tr_time"]:.3f}\t{means["te_time"]:.3f}\n')
+            csv.flush()
+
+
+
+if __name__ == '__main__':
+
+    qp.environ['SAMPLE_SIZE'] = 500
+    qp.environ['N_JOBS'] = -1
+    n_bags_val = 100
+    n_bags_test = 100
+    result_dir = f'results/ucimulti'
+
+    os.makedirs(result_dir, exist_ok=True)
+
+    global_result_path = f'{result_dir}/allmethods'
+    with open(global_result_path + '.csv', 'wt') as csv:
+        csv.write(f'Method\tDataset\tMAE\tMRAE\tSUCCESS\tTR-TIME\tTE-TIME\n')
+
+    for method_name, quantifier, param_grid in METHODS:
+        run_experiment(method_name, quantifier, param_grid)
+
+    show_results(global_result_path)
+
---
+++ model.py
@@ -0,0 +1,122 @@
+import functools
+from functools import cache
+
+import numpy as np
+import quapy as qp
+from quapy.data import LabelledCollection
+from quapy.method.aggregative import AggregativeQuantifier
+from quapy.method.base import BaseQuantifier
+from scipy.stats import chi2
+from sklearn.utils import resample
+from abc import ABC, abstractmethod
+
+
+class ConfidenceRegion:
+    def __init__(self, mean, cov, critical):
+        self.mean = mean
+        self.cov = cov
+        self.critical = critical
+
+    @functools.cached_property
+    def precision_matrix(self):
+        try:
+            prec_matrix = np.linalg.inv(self.cov)
+        except:
+            prec_matrix = None
+        return prec_matrix
+
+    def within(self, true_prev):
+        if self.precision_matrix is None:
+            return False
+
+        # Mahalanobis distance
+        diff = true_prev - self.mean
+        d_M_squared = np.dot(np.dot(diff.T, self.precision_matrix), diff)  # d_M^2
+        within_elipse = d_M_squared <= self.critical
+        return within_elipse
+
+class WithCIAbstract(ABC):
+    @abstractmethod
+    def quantify_ci(self, instances):
+        ...
+
+# class WithCI(WithCIAbstract, BaseQuantifier):
+#
+#     def __init__(self, quantifier:BaseQuantifier, n_samples=100, sample_size=0.5, random_state=None):
+#         assert n_samples > 1, f'{n_samples=} must be > 1'
+#         assert (type(sample_size)==float and sample_size > 1) or (type(sample_size)==int and sample_size>1), \
+#             f'wrong value for {sample_size=}; specify a float (a proportion of the original size) or an integer'
+#         self.quantifier = quantifier
+#         self.n_samples = n_samples
+#         self.sample_size = sample_size
+#         self.random_state = random_state
+#
+#     def fit(self, data: LabelledCollection):
+#         return self.quantifier.fit(data)
+#
+#     def quantify(self, instances):
+#         prev_mean, self.prev_cov_last = self.quantify_ci(instances)
+#         return prev_mean
+#
+#     def quantify_ci(self, instances):
+#         prevs = []
+#         for i in range(self.n_samples):
+#             sample_i = resample(instances, n_samples=self.sample_size, random_state=self.random_state)
+#             prev_i = self.quantifier.quantify(sample_i)
+#             prevs.append(prev_i)
+#         prevs = np.asarray(prevs)
+#         prev_mean = prevs.mean(axis=0)
+#         prev_cov  = np.cov(prevs)
+#         return prev_mean, prev_cov
+
+
+class WithCIAgg(WithCIAbstract, AggregativeQuantifier):
+
+    def __init__(self, quantifier: AggregativeQuantifier, n_samples=100, sample_size=0.5, confidence_level=0.95, random_state=None):
+        assert n_samples > 1, f'{n_samples=} must be > 1'
+        assert (type(sample_size) == float and sample_size > 0) or (type(sample_size) == int and sample_size > 1), \
+            f'wrong value for {sample_size=}; specify a float (a proportion of the original size) or an integer'
+        self.quantifier = quantifier
+        self.n_samples = n_samples
+        self.sample_size = sample_size
+        self.random_state = random_state
+
+    def aggregation_fit(self, classif_predictions: LabelledCollection, data: LabelledCollection):
+        return self.quantifier.aggregation_fit(classif_predictions, data)
+
+    def aggregate(self, classif_predictions: np.ndarray):
+        prev_mean, self.prev_cov_last = self.aggregate_ci(classif_predictions)
+        return prev_mean
+
+    def aggregate_ci(self, classif_predictions: np.ndarray, confidence_level=None):
+        if confidence_level is None:
+            confidence_level = self.confidence_level
+
+        if type(self.sample_size)==float:
+            n_samples = int(classif_predictions.shape[0] * self.sample_size)
+        else:
+            n_samples = self.sample_size
+
+        prevs = []
+        for i in range(self.n_samples):
+            sample_i = resample(classif_predictions, n_samples=n_samples, random_state=self.random_state)
+            prev_i = self.quantifier.aggregate(sample_i)
+            prevs.append(prev_i)
+
+        prevs = np.asarray(prevs)
+        mean = prevs.mean(axis=0)
+        cov = np.cov(prevs, rowvar=False, ddof=1)
+
+        # critical chi-square value
+        n_features = prevs.shape[1]  # number of variables is the number of classes
+        chi2_val = chi2.ppf(confidence_level, df=n_features)  # shoudn't this be n_features-1? they sum up to 1 so...
+
+        return ConfidenceRegion(mean, cov, chi2_val)
+
+
+    def fit(self, data: LabelledCollection, fit_classifier=True, val_split=None):
+        return self.quantifier.fit(data, fit_classifier, val_split)
+
+    def quantify_ci(self, instances, confidence_level=0.95):
+        predictions = self.quantifier.classify(instances)
+        return self.aggregate_ci(predictions, confidence_level=confidence_level)
